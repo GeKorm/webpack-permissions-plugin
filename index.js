ee539536b3dd7cfc12010b855cae1fb29eecbaad
GeKorm <mail@gekorm.com>
2019-09-01 17:55:37 +0100

    Do not fail when a directory doesn't exist
    Logs a warning and continues compilation

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 const fs = require('fs');
 const FileHound = require('filehound');
 
+const pluginName = 'WebpackPermissionsPlugin';
+
+const warn = (logger, path) => {
+  const errorMessage = `Directory not found ${path}`;
+  if (logger) {
+    logger.warn(errorMessage);
+  } else {
+    console.warn(`WebpackPermissionsPlugin: ${errorMessage}`);
+  }
+};
+
 function PermissionsOutputPlugin(options) {
   this.options = options;
 }
 
 PermissionsOutputPlugin.prototype.apply = function(compiler) {
   const changeFilePermissions = () => {
+    const logger =
+      compiler.getInfrastructureLogger &&
+      compiler.getInfrastructureLogger(pluginName);
+
     if (this.options.buildFolders) {
       for (const dir of this.options.buildFolders) {
+        const path = dir.path || dir;
+        if (!fs.existsSync(dir)) {
+          warn(logger, path);
+          return;
+        }
+
         const dirs = FileHound.create()
-          .path(dir.path || dir)
+          .path(path)
           .directory()
           .findSync();
         const files = FileHound.create()
-          .path(dir.path || dir)
+          .path(path)
           .not()
           .directory()
           .findSync();
@@ -45,7 +66,7 @@ PermissionsOutputPlugin.prototype.apply = function(compiler) {
     compiler.hooks.done.tap.bind(compiler.hooks.done);
 
   if (webpackTap) {
-    webpackTap('WebpackPermissionsPlugin', changeFilePermissions);
+    webpackTap(pluginName, changeFilePermissions);
   } else {
     compiler.plugin('done', changeFilePermissions);
   }
